7345f17835557ed20da9e1ec607253dd5f9f1660
OlgaLugacheva <lugachevaolga@gmail.com>
2023-01-20 23:13:16 +0300

    description, validation, response entity

--- src/main/java/ru/sky/recipebook/model/Recipe.java
+++ src/main/java/ru/sky/recipebook/model/Recipe.java
@@ -1,6 +1,5 @@
 package ru.sky.recipebook.model;
 
-import io.swagger.v3.oas.models.security.SecurityScheme;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
